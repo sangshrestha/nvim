85c940fcf5a756376fc4936f3c22914c77512caf
sangshrestha <sang.socials@gmail.com>
2024-07-21 17:29:20 +0100

    feat: add telescope

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,13 @@
 return {
-  "sainnhe/everforest",
-  lazy = false,
-  priority = 999,
-  config = function()
-    vim.cmd.colorscheme("everforest")
-  end,
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 999,
+    config = function()
+      vim.g.everforest_background = "hard"
+      vim.g.everforest_transparent_background = 2
+      vim.g.everforest_ui_contrast = "high"
+      vim.cmd.colorscheme("everforest")
+    end,
+  },
 }
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[f]ind [f]iles" })
+    vim.keymap.set("n", "<leader>fF", builtin.git_files, { desc = "[f]ind git [F]iles" })
+    vim.keymap.set("n", "<leader>ss", builtin.grep_string, { desc = "[s]earch [s]tring" })
+    vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+    -- Shortcut for searching your Neovim configuration files
+    vim.keymap.set("n", "<leader>fc", function()
+      builtin.find_files({ cwd = vim.fn.stdpath("config") })
+    end, { desc = "[f]ind [c]onfig files" })
+  end,
+}
