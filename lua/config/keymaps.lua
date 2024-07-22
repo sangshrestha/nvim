4c610c4d35e7cd04674c1e49e2821616c36a0f01
sangshrestha <sang.socials@gmail.com>
2024-07-22 09:12:42 +0100

    feat: leader paste

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,6 @@
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Paste without losing yank register
+vim.keymap.set("x", "<leader>p", [["_dP]])
