28211f88d261d31837aac7373055ca9e000cf125
sangshrestha <sang.socials@gmail.com>
2024-07-28 11:31:21 +0100

    feat: toggleterm size adjust

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,10 +1,18 @@
 return {
   "akinsho/toggleterm.nvim",
   config = function()
+    local computeHorizontalSize = function()
+      local width = vim.api.nvim_win_get_width(0) * 2
+
+      if width - 100 > 100 then
+        return width - 100
+      else
+        return width
+      end
+    end
+
     require("toggleterm").setup({
-      size = function()
-        return math.max(vim.api.nvim_win_get_width(0) * 2 - 90, 80)
-      end,
+      size = computeHorizontalSize,
       open_mapping = [[<c-\>]],
       persist_size = false,
       terminal_mappings = true,
@@ -12,7 +20,7 @@ return {
       autochdir = true,
       start_in_insert = true,
       direction = "vertical",
-      shading_factor = -5,
+      shading_factor = -6,
     })
   end,
 }
