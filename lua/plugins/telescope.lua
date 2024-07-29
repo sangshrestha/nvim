5ad8576b3d4a19a68374685fda4e41be5601f521
sangshrestha <sang.socials@gmail.com>
2024-07-29 08:18:30 +0100

    feat: more keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,7 @@ return {
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[f]ind [f]iles" })
     vim.keymap.set("n", "<leader>fF", builtin.git_files, { desc = "[f]ind git [F]iles" })
     vim.keymap.set("n", "<leader>ss", builtin.grep_string, { desc = "[s]earch [s]tring" })
+    vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Search [f]iles opened [r]ecently" })
     vim.keymap.set("n", "<leader>bb", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
     -- Shortcut for searching your Neovim configuration files
